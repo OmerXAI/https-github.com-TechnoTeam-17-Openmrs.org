ff571cc365dd55ea77f3baff1b7d83c2cc324316
OmerXAI <omer.xxx.ai@gmail.com>
2023-09-10 11:31:32 +0200

    elementler ayrildi. clean code yapildi chrome da calisiyor artik

--- src/us_10_wrongClock.java
+++ src/us_10_wrongClock.java
@@ -1,4 +1,6 @@
 import Utility.BaseDriver;
+import Utility.MyFunc;
+import Utility.mrs_elements;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
@@ -6,17 +8,14 @@ import org.testng.annotations.Test;
 public class us_10_wrongClock extends BaseDriver {
     @Test
     public void test(){
-        WebElement app=driver.findElement
-                (By.xpath("//*[@id='appointmentschedulingui-homeAppLink-appointmentschedulingui-homeAppLink-extension']"));
-            app.click();
 
-        WebElement app2=driver.findElement
-                (By.xpath("//*[@id=\"appointmentschedulingui-manageAppointments-app\"]/i"));
-        app2.click();
-        //*[@id="patient-search"]
-        WebElement app3=driver.findElement
-                (By.xpath("//*[@id=\"patient-search\"]"));
-        app3.sendKeys("Nergis hnm dan gelecek bilgiler");
+        mrs_elements elemans=new mrs_elements();
+
+        elemans.app.click();
+        MyFunc.Bekle(3);
+        elemans.app2.click();
+        MyFunc.Bekle(3);
+        elemans.app3.sendKeys("Nergis hnm dan gelecek bilgiler");
 
 
     }
