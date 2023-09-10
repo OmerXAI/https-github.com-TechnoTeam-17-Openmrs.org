f1bfd21e2b862436534a97d7022102357ac6609a
NergisRecel <nergisrecel@hotmail.com>
2023-09-10 23:39:38 +0200

    mrs_elements classina hasta kayit ile ilgili elementler eklendi. User story 4 update edildi

--- src/Utility/mrs_elements.java
+++ src/Utility/mrs_elements.java
@@ -30,6 +30,45 @@ public class mrs_elements {
     public WebElement app2;
     @FindBy(xpath="//*[@id='patient-search']")
     public WebElement app3;
+    @FindBy(xpath ="//a[@id='referenceapplication-registrationapp-registerPatient-homepageLink-referenceapplication-registrationapp-registerPatient-homepageLink-extension']")
+    public WebElement patientRegistarion;
+    @FindBy(xpath="//*[@id='fr89-field']")
+    public WebElement nameReg;
+    @FindBy(xpath="//*[@id='fr2310-field']")
+    public WebElement familyNameReg;
+    @FindBy(xpath="//*[@id='next-button']")
+    public WebElement nextReg;
+    @FindBy(xpath="//*[@value='F']")
+    public WebElement female;
+    @FindBy(xpath="//*[@id='birthdateDay-field']")
+    public WebElement birthDay;
+    @FindBy(xpath="//*[@id='birthdateMonth-field']")
+    public WebElement birthMonth;
+    @FindBy(xpath="//*[@id='birthdateYear-field']")
+    public WebElement birthYear;
+    @FindBy(xpath="//*[@id='address1']")
+    public WebElement address;
+
+    @FindBy(xpath="//*[@id='cityVillage']")
+    public WebElement city;
+    @FindBy(xpath="//*[@name='country']")
+    public WebElement country;
+    @FindBy(xpath="//*[@name='phoneNumber']")
+    public WebElement phone;
+    @FindBy(xpath="//*[@id='relationship_type']")
+    public WebElement relationship;
+    @FindBy(xpath="//*[@placeholder='Person Name']")
+    public WebElement personName;
+    @FindBy(xpath="//*[@id='submit']")
+    public WebElement confirm;
+
+
+
+
+
+
+
+
 
 
 
