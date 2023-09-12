5fa81ee6d90d468d49e6a37d0666dc754f4e7d97
AyGulll <abilovaygul@gmail@com>
2023-09-12 14:31:17 +0100

    1. deneme

---
+++ src/US_06_HastaListesindeArama.java
@@ -0,0 +1,11 @@
+import Utility.BaseDriver;
+import org.testng.annotations.Test;
+
+public class US_06_HastaListesindeArama extends BaseDriver {
+
+   @Test
+    public void Test(){
+
+       login();
+    }
+}
